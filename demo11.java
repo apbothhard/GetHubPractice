118ac96acc5b194b283001cf6af9ddf48d423300
apbothhard <kiranpanchgade@gmail.com>
2024-11-27 23:05:18 +0530

    https://github.com/apbothhard/GetHubPractice.git

---
+++ demo11.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class demo11
+{
+public static void main(String[] args) 
+{
+	int num=4;
+	int fact=1;
+	for(int i=num;i>=1;i--) {
+		fact=fact*1;
+	}
+	System.out.println(fact);
+}
+}
