283749c4170968be08b19720b2cecd2c8445ac46
apbothhard <kiranpanchgade@gmail.com>
2024-11-27 23:20:25 +0530

    demohello

--- demo12.java
+++ demo12.java
@@ -5,15 +5,15 @@ public class demo12
 public static void main(String[] args) 
 {
 int num1=10;
-int num2=20;
+int num=20;
 System.out.println("before swap: ");
 System.out.println("number1: " +num1);
-System.out.println("number2: "+num2);
-num1=num1+num2; //10=10+20
-num2=num1-num2;//20=10+20
-num1=num1-num2;//10=10-20
+System.out.println("number: "+num);
+num1=num1+num; //10=10+20
+num=num1-num;//20=10+20
+num1=num1-num;//10=10-20
 System.out.println("After swap: ");
 System.out.println("number1: "+num1);
-System.out.println("number2: "+num2);
+System.out.println("number: "+num);
 }
 }
